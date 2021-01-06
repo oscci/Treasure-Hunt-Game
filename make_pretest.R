6db241ca67e5c72e0cb99a522938674de74b2cc7
Dorothy Bishop <dorothybishop@Dorothys-MacBook-Air-2.local>
2021-01-06 18:32:18 +0000

    For Registered Report: 24 items of above/below, and 24 of between. Need to sort out coding of correct item for 'between'; otherwise good.

--- make_pretest.R
+++ make_pretest.R
@@ -2,7 +2,7 @@
 
 # Used to make a set of 8 items with complex level 4 structure, but easily could be adapted for other item types
 
-(#Couldn't seem to get this to work as notebook?, hence basic R script)
+#Couldn't seem to get this to work as notebook?, hence basic R script
 
 
 # Pictures from Multipic
@@ -10,21 +10,22 @@
 #  Reference: Duñabeitia, J.A., Crepaldi, D., Meyer, A.S., New, B., Pliatsikas, C., Smolka, E., & Brysbaert, M. (2017). MultiPic: A standardized set of 750 drawings with norms for six European languages. Quarterly Journal of Experimental Psychology. doi:10.1080/17470218.2017.1310261
 
 #NB - currently directories for reading pictures and writing items are hard-coded here.
-
-require(magick)
-picdir <- "/Users/dorothybishop/deevybee_repo/magic images/colored PNG/"
-mydir <- "/Users/dorothybishop/deevybee_repo/magic images/assembled_pics/"
+library(groundhog)
+groundhog.library(magick,"2020-12-01")
+picdir <- "~/Dropbox/magic images/colored PNG/"
+mydir <- "~/Dropbox/magic images/assembled_pics/"
 
 myitems<-read.csv(paste0(picdir,'multipic_items.csv'))
 
-NB! picture1 has been overwritten with blank
+#NB! picture1 has been overwritten with blank
 
+# For pretest + posttest we will need 24 unique items 
 # 
 #Make data frame to hold sentences
-sentencedf <- data.frame(matrix(NA,nrow=8,ncol=2))
+sentencedf <- data.frame(matrix(NA,nrow=24,ncol=2))
 colnames(sentencedf)<-c('Sentence','Correct')
 # We now want to build sentences and a set of 3-picture items for each sentence.
-# We will have 2 sentences of each of these types
+# We will have 6 sentences of each of these types
 # 
 # The X is above the Y and next to the Z
 # The X is below the Y and next to the Z
@@ -51,13 +52,13 @@ assemblepics <- function(itemname,top1,top2,bot1,bot2){
   image_write(imageall, path=itemname, format = "png")
 }
 
-nitem=8
+nitem=24
 for (i in 1:nitem){
   mynum1<-(3*(i-1)+1)
   Xword <- myitems$pic[mynum1]
   Yword <- myitems$pic[(mynum1+1)]
   Zword <- myitems$pic[(mynum1+2)]
-  typelist <- c(1,2,3,4,3,1,4,2) #quasirandom order of items
+  typelist <- c(1,2,3,4,3,2,4,1,3,1,2,4,4,3,2,1,2,1,3,4,3,1,4,2) #quasirandom order of items
   sentencedf$Correct <- LETTERS[typelist]
   senttypes <- c('The X is above the Y and next to the Z',
     'The X is below the Y and next to the Z',
@@ -69,8 +70,6 @@ for (i in 1:nitem){
   mysent1 <- gsub("Z",Zword,mysent1)
   sentencedf$Sentence[i]<-mysent1
  
-
-picdir <- "/Users/dorothybishop/deevybee_repo/magic images/colored PNG/"
 imageX<-image_read(paste0(picdir,'Picture_',myitems$num[(mynum1)],'.png'))
 imageY<-image_read(paste0(picdir,'Picture_',myitems$num[(mynum1+1)],'.png'))
 imageZ<-image_read(paste0(picdir,'Picture_',myitems$num[(mynum1+2)],'.png'))
@@ -99,14 +98,6 @@ bot1<-imX
 bot2<-imZ
 assemblepics(itemname,top1,top2,bot1,bot2)
 
-
-# Array type C: The X is next to the Y and above the Z
-itemname <- paste0(mydir,'Item_',i,'C.png')
-top1<-imX
-top2<-imY
-bot1<-imZ
-bot2<-blank
-assemblepics(itemname,top1,top2,bot1,bot2)
 
 # Array type C: The X is next to the Y and above the Z
 itemname <- paste0(mydir,'Item_',i,'C.png')
@@ -147,5 +138,89 @@ gdf$answer[w]<- paste0("Item_",1:nitem,sentencedf$Correct,'.png')
 
 write.csv(gdf,paste0(mydir,'GorillaSheet.csv'),row.names=F)
 
+#######################################################################
+# Now do the same but for 'between' sentences
+
+
+#function to assemble pics
+assemblepicsb <- function(itemname,imX,imY,imZ){
+  imgtop <- c(imX,imY,imZ)
+  imgtop <- image_append(image_scale(imgtop, "x100"))
+  imageall<-image_append(image_scale(imgtop, "x100"),stack=F)
+  imageall<-image_border(image_background(imageall, "white"), "black", "2x3")
+  image_write(imageall, path=itemname, format = "png")
+}
+
+sentencedfb <- data.frame(matrix(NA,nrow=24,ncol=2))
+colnames(sentencedfb)<-c('Sentence','Correct')
+
+nitem=24
+typelist <- c(1,2,3,4,3,2,4,1,3,1,2,4,4,3,2,1,2,1,3,4,3,1,4,2) #quasirandom order of items
+
+for (i in 1:nitem){
+  mynum1<-(3*(i-1)+1)
+  Xword <- myitems$pic[mynum1]
+  Yword <- myitems$pic[(mynum1+1)]
+  Zword <- myitems$pic[(mynum1+2)]
+ 
+  sentencedfb$Correct <- LETTERS[typelist]
+  
+ 
+  senttypes <- c('The X is between the Y and the Z') #just one sentence type, but distractors can include:
+  # X Y Z
+  # Y X Z *
+  # X Z Y
+  # Y Z X
+  thissent<-senttypes[1]
+  mysent1 <- gsub("X", Xword, thissent)
+  mysent1 <- gsub("Y",Yword,mysent1)
+  mysent1 <- gsub("Z",Zword,mysent1)
+  sentencedfb$Sentence[i]<-mysent1
+  
+  imageX<-image_read(paste0(picdir,'Picture_',myitems$num[(mynum1)],'.png'))
+  imageY<-image_read(paste0(picdir,'Picture_',myitems$num[(mynum1+1)],'.png'))
+  imageZ<-image_read(paste0(picdir,'Picture_',myitems$num[(mynum1+2)],'.png'))
+  imX<-image_append(image_scale(imageX, "100"))
+  imY<-image_append(image_scale(imageY, "100"))
+  imZ<-image_append(image_scale(imageZ, "100"))
+  
+
+  itemname <- paste0(mydir,'Item_',i,'A.png')
+  #array type A : X Y Z
+  top1<-imX
+  top2<-imY
+  top3<-imZ
+  
+  #function to assemble pics
+
+  assemblepicsb(itemname,top1,top2,top3)
+  
+  #array type B: Y X Z
+  itemname <- paste0(mydir,'Item_',i,'B.png')
+  top1<-imY
+  top2<-imX
+  top3<-imZ
+
+  assemblepicsb(itemname,top1,top2,top3)
+  
+  #array type C: Y X Z
+  itemname <- paste0(mydir,'Item_',i,'C.png')
+  
+  top1 <- imX
+  top2 <- imZ
+  top3 <- imY
+  
+  assemblepicsb(itemname,top1,top2,top3)
+  
+  #array type D: Y Z X
+  itemname <- paste0(mydir,'Item_',i,'D.png')
+  top1<-imY
+  top2<-imZ
+  top3<-imX
+  
+  assemblepicsb(itemname,top1,top2,top3)
+}
+
+write.csv(sentencedfb,paste0(mydir,'SentenceList.csv'),row.names=T)
 
 
